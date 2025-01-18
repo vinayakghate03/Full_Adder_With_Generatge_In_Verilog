bb13562c0b5e93b12b8f22a262c8303e82bc7a01
Vinayak_Ghate_03 <131643851+vinayakghate03@users.noreply.github.com>
2025-01-18 11:32:24 +0530

    FULL_ADDER.v

--- FULL_ADDER.v
+++ FULL_ADDER.v
@@ -6,7 +6,7 @@
 // Create Date: 17.01.2025 19:14:26
 // Design Name: 
 // Module Name: FULL_ADDER
-// Project Name: Full adders with generate using instatiation modules
+// Project Name: Full adders with generate using instantiation modules
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -20,63 +20,63 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module FULL_ADDER(   // this is my sub module
-             input X,Y,Z,
+module FULL_ADDER(   // this is my submodule
+             input X, Y, Z,
              output SUM, CARRY
     );
     
 
-//    we are using continus assigment statement with for of implicity assigment statement for the
+//We are using continuous assignment statement with for of implicity assignment statement for the
    assign SUM = X ^ Y ^ Z;
    assign CARRY = (X & Y)|(Y & Z)|(X & Z);
 
 endmodule
 
-//we are create the generate module to genertae the N-number of the instatiation for create the N-number of the adders 
+//we are creating the generate module to generate the N-number of the instantiation for create the N-number of the adders 
 
 module generator #(parameter N_FA = 0) // assign defult value
      (
        input a,b,c,
-       output sum,carry);
+       output sum, carry);
        
-//       we are using the generate veriable to generate the N number of the FULL Adders this is pass using parameters
+//We are using the generate variable to generate the N number of the FULL Adders this is passed using parameters
   
-      genvar  i ;// create the  veriable
+      genvar  i ;// create the  variable
        
        generate // keyword to generate N
-        // this is for loop generator
+        //This is for-loop generator
           for( i = 0 ;i < N_FA; i = i + 1) begin
            
-//         we are create instance for the N n umber of FULL ADDER generated which is know as the inner instacne 
+//         we are creating instance for the N number of FULL ADDER generated which is known as the inner instance 
 
              FULL_ADDER My_Ganerate_Instance(   // this is the Sub module instantiation
              
-                    .X(a), .Y(b), .Z(c), .SUM(sum), .CARRY(carry)); // we connect the pins by port name oerde 
+               .X(a), .Y(b), .Z(c), .SUM(sum), .CARRY(carry)); // we connect the pins by port name order 
            end
        endgenerate
               
 endmodule             
                
-//we are create the top module as test bench for test the adders 
+//we are creating the top module as a test bench for testing the adders 
 
 module top_test;
-      reg A,B,C;
-      wire SUM,CARRY;
+      reg A, B, C;
+      wire SUM, CARRY;
       integer i ; // for value assign
-//   we are create the top instance of the generate sub modules
+//We are creating the top instance of the generated submodules
     
-    generator #(.N_FA(5))   // this is assign the number of the generate instacne inside of the top module
-             Top_Instance(   // top moudle inst created with name port connections
+  generator #(.N_FA(5))   //This is assigned the number of the generated instance inside of the top module
+  Top_Instance(   // top module inst created with name port connections
                .a(A), .b(B), .c(C), .sum(SUM), .carry(CARRY));       
              
              
-//          there using the inital block to assign the vlaues 
+//          there using the initial block to assign the values 
 
     initial begin
       
-//      we are using for loop we assign the values in binary form
+//We are using for loop we assign the values in binary form
        for( i= 0 ; i <8; i = i + 1) begin
-         {A,B,C} = i;   // this is the concatinations of the veriable to assign the vlaues 
+         {A, B, C} = i;   //This is the concatenation of the variable to assign the values 
            #10;   // time units
            display_add();
          end
@@ -87,10 +87,10 @@ module top_test;
      $display("\t FULL ADDER OUTPUT");
      
  
- // create the task for the display the value on the consul
+ //Create the task for the display of the value on the consul
    task display_add();
        begin
-       $display("A = %d, B = %d, C = %d, SUM = %d, CARRY = %d",A,B,C,SUM,CARRY);
+         $display("A = %d, B = %d, C = %d, SUM = %d, CARRY = %d",A, B, C, SUM, CARRY);
       end
     endtask
 endmodule
